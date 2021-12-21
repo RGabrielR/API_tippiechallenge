735e66384c9e53eee85bb03ca527df5a2421c293
Gabriel <ivangabrielalabi@gmail.com>
2021-12-21 18:38:09 -0300

    fix cors errors

--- server.js
+++ server.js
@@ -3,8 +3,14 @@ const app= express();
 const employeesData= require('./data');
 const port = process.env.PORT || 3001;
 const cors= require('cors');
-
 app.use(cors());
+app.use(function(req, res, next) {
+res.header("Access-Control-Allow-Origin", '*');
+res.header("Access-Control-Allow-Credentials", true);
+res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+next();
+});
 app.get('/', (req, res) => {
     res.json(employeesData)
 })
