749f3dd688b3564789e0fb8611a3295c7eae5a02
Gabriel <ivangabrielalabi@gmail.com>
2021-12-22 02:02:52 -0300

    fixing errors

--- server.js
+++ server.js
@@ -9,8 +9,9 @@ res.header("Access-Control-Allow-Credentials", true);
 res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
 res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
 res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+next();
 });
-app.get('/', cors(), (req, res) => {
+app.get('/', (req, res) => {
     res.json(employeesData).catch(error =>{
       res.json(error)
 })
