7a995aab215783a886321c3c3ed395430d4e1215
Gabriel <ivangabrielalabi@gmail.com>
2021-12-22 01:53:07 -0300

    add cache control

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const port = process.env.PORT || 3001;
 const cors= require('cors');
 app.use(cors());
 app.use(function(req, res, next) {
-res.header("Access-Control-Allow-Origin", '*');
 res.header("Access-Control-Allow-Credentials", true);
 res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
 res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+res.header("Cache-Control", "no-cache, no-store, must-revalidate");
 next();
 });
 app.get('/', (req, res) => {
